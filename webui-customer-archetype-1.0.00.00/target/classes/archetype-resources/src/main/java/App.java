0e19d216dd1d4845cb5321704f891ef1ca1bbc51
Nikola Bikic <bikicnikola14@gmail.com>
2021-04-05 00:08:46 +0200

    upload custom web ui archetype arcitype

---
+++ webui-customer-archetype-1.0.00.00/target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.payengineer.webui.webui_customer_archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
